bcf47277f9c473f7f2fd985ccfb8fcd1d34bb8e2
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-05-28 20:05:06 +0200

    Homework36 some changes

--- src/main/java/homework/homework36/DrinksAutomat.java
+++ src/main/java/homework/homework36/DrinksAutomat.java
@@ -1,30 +1,27 @@
 package homework.homework36;
-
-import lessons.exceptionsHandling.ErrorCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DrinksAutomat {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorCalculator.class);
-    static String[] drinks = new String[]{"Сок", "Кофе", "Чай"};
-    static Scanner scanner = new Scanner(System.in);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DrinksAutomat.class);
+    private static String[] drinks = new String[]{"Сок", "Кофе", "Чай"};
+   private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         LOGGER.info("Пришел клиент");
         LOGGER.info("\nВведите номер напитка\n1 - Сок\n2 - Кофе\n3 - Чай");
+        int number =0;
         try {
-            int number = scanner.nextInt();
-            LOGGER.info("Напиток {} готовится", drinks[number-1]);
+            number = Integer.parseInt(scanner.nextLine()) ;
+            String drink = drinks[number-1];
+            LOGGER.info("Напиток {} готовится", drink);
 
-        } catch (InputMismatchException exception) {
-            LOGGER.error("Ввели нецелое число или буквы!");
-            LOGGER.error(exception.getLocalizedMessage());
+        } catch (NumberFormatException exception ) {
+            LOGGER.error("Неверный формат ввода напитка Excetion:{}",exception.getMessage());
         }
         catch (ArrayIndexOutOfBoundsException exception){
-            LOGGER.error("Номер выходит за пределы массива");
+            LOGGER.error("Номер выходит за пределы массива, неправильный номер напитка");
             LOGGER.error(exception.getLocalizedMessage());
         }
         finally {
